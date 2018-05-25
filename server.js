6b2cfadadd675318afa5c9338b999937c3a4b164
Venan24 <venanosmic24@gmail.com>
2018-05-25 02:56:44 +0200

    Reading employees from database

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/employeeMng');
+
+var Employee = mongoose.model('Employee', mongoose.Schema({
+  name:String,
+	dept:String,
+	area:String,
+	status:String,
+	contact:String,
+	salary:String
+}));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client'));
+//app.use(express.static(__dirname + '/client'));
 
-app.listen(3000, function(){
-  console.log("Radiiiii");
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+
+app.get('/restic/employees', function(req, res){
+  Employee.find(function(err, employees){
+    if(err)
+      res.send(err);
+    res.json(employees);
+  })
+})
+
+var listener = app.listen(3000, function(){
+  console.log("Server je pokrenut na: " + listener.address().port + " portu");
+})
