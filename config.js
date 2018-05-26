c293f2427d4db74388a6200007766546c5bc1db1
Venan24 <venanosmic24@gmail.com>
2018-05-26 18:28:42 +0200

    Authentication with hashing and JWT

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'secret': 'testpromjena',
+    'secret': 'OvoJeNajjaciSecretKojiSamSmislio##1337##',
     'database': 'mongodb://localhost:27017/employeeMng'
 };
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 // ======================= \\
 //        Packages         \\
 // ======================= \\
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const mongoose = require('mongoose');
-const config = require('./config'); // Get configurations
-const bcrypt = require('bcrypt');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
-mongoose.connect(config.database); // Connect to db
-
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // Get configurations
 var Employee = require('./app/models/employee'); // Mongoose employees model
 var User = require('./app/models/user'); // Mongoose users model
+
+mongoose.connect(config.database); // Connect to db
+app.set('superSecret', config.secret); // secret variable
 
 app.use(bodyParser.urlencoded({extended:false})); // body-parser => we can get info from POST/URL parameters
 app.use(bodyParser.json());
@@ -21,11 +23,85 @@ app.use(bodyParser.json());
 //         Routes          \\
 // ======================= \\
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send("Hello! The API is at /api");
+});
+
+// ========================================================= \\
+//       Get an instance of the router for api routes        \\
+// ========================================================= \\
+var apiRoutes = express.Router();
+
+//Authentication - No Middleware needed
+apiRoutes.post('/authenticate', function(req, res){
+	var username = req.body.username;
+	var enteredPassword = req.body.password;
+
+	User.findOne({username:username}, function(err, users){
+		if(err)
+			res.send("No user found");
+		bcrypt.compare(enteredPassword, users.password, function(err, resp) {
+			if(resp===true){
+				const payload = {
+					//__id: users.__id
+					username: users.username,
+					firstname: users.firstname,
+					lastname: users.lastname,
+					admin: users.admin
+				};
+				var token = jwt.sign(payload, app.get('superSecret'), {
+					expiresIn : 60*60*24 //24 hours valid token
+				});
+				res.json({
+					success: true,
+          message: 'Successfully Logged in!',
+          token: token
+        });
+			}else{
+				res.json({message: "Wrong password"})
+			}
+		});
+	});
+});
+
+// ========================================================== \\
+//      Route middleware to authenticate and check token      \\
+// ========================================================== \\
+apiRoutes.use(function(req, res, next){
+
+	// Check header/URL/POST parameters for token
+	var token = req.body.token || req.params.token || req.headers['x-access-token'];
+
+	// Decode token
+	if(token){
+		// Verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function(err, decoded){			
+			if(err){
+				return res.json({ success: false, message: 'Failed to authenticate token.' });		
+			}else{
+				// If everything is good, save to request for use in other routes
+				req.decoded = decoded;	
+				next();
+			}
+		});
+	}else{
+		// If there is no token
+		// Return an error
+		return res.status(403).send({ 
+			success: false, 
+			message: 'No token provided.'
+		});
+	}
+});
+
+// ========================== \\
+//    Authenticated routes    \\
+// ========================== \\
+apiRoutes.get('/', function(req, res){
+	res.json({ message: 'Welcome!' });
 });
 
 //Get all employees
-app.get('/restic/employees', function(req, res){
+apiRoutes.get('/employees', function(req, res){
   Employee.find(function(err, employees){
     if(err)
       res.send(err);
@@ -34,7 +110,7 @@ app.get('/restic/employees', function(req, res){
 });
 
 //Get employee by id
-app.get('/restic/employees/:id', function(req, res){
+apiRoutes.get('/employees/:id', function(req, res){
 	Employee.findOne({_id:req.params.id}, function(err, employee){
 		if(err)
 			res.send(err);
@@ -43,7 +119,7 @@ app.get('/restic/employees/:id', function(req, res){
 });
 
 //Create new employee
-app.post('/restic/employees', function(req, res){
+apiRoutes.post('/employees', function(req, res){
 	Employee.create( req.body, function(err, employees){
 		if(err)
 			res.send(err);
@@ -52,7 +128,7 @@ app.post('/restic/employees', function(req, res){
 });
 
 //Create new user
-app.post('/restic/users', function(req, res){
+apiRoutes.post('/users', function(req, res){
 	var username = req.body.username;
 	var firstname = req.body.firstname;
 	var lastname = req.body.lastname;
@@ -68,29 +144,8 @@ app.post('/restic/users', function(req, res){
 	});
 });
 
-//Authentificate login user
-app.post('/restic/login', function(req, res){
-	var username = req.body.username;
-	var enteredPassword = req.body.password;
-
-	User.findOne({username:username}, function(err, users){
-		if(err)
-			res.send("No user found");
-		bcrypt.compare(enteredPassword, users.password, function(err, resp) {
-			if(resp===true){
-				console.log("PASSWORD MATCH!!!");
-				res.json({username: users.username, firstname: users.firstname, lastname: users.lastname, _id: users.id});
-			}else{
-				console.log("WRONG PASSWORD!!!");
-				res.send("WRONG PW")
-			}
-		});
-	});
-	
-});
-
 //Get all users Just4TEsting
-app.get('/restic/users', function(req, res){
+apiRoutes.get('/users', function(req, res){
   User.find(function(err, users){
     if(err)
       res.send(err);
@@ -98,9 +153,11 @@ app.get('/restic/users', function(req, res){
   })
 });
 
+app.use('/api', apiRoutes);
+
 // ======================= \\
 //      Start Server       \\
 // ======================= \\
 var listener = app.listen(3000, function(){
-  console.log("Server je pokrenut na: " + listener.address().port + " portu");
-})
+  console.log("Started at http:localhost:" + listener.address().port + " port");
+});
