3cc99ff52ae1959b40082c9b9377f3a0eb625e94
Venan24 <venanosmic24@gmail.com>
2018-05-27 17:38:56 +0200

    mlab db connection

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret': 'OvoJeNajjaciSecretKojiSamSmislio##1337##',
-    'database': 'mongodb://localhost:27017/employeeMng'
+    'database': 'mongodb://localhost:27017/employeeMng',
+    //'livedb': 'mongodb://empuser:emp12345@ds233500.mlab.com:33500/employmng'
 };
